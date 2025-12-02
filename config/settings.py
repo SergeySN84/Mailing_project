11a4c6e25a9a0c56d2b6fc58acd20b71ba72186f
SergeySN84 <Sergsvircuk@mail.ru>
2025-12-02 21:44:00 +0700

    Создание структуры проекта.

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n4iug74xxzz)m)#=bf(^vp9n12p0xu_kor_y^mgd1*z6(my5%s'
+SECRET_KEY = 'django-insecure-^m7ao7y7wdj$437@*aywt#c$^=%l^l0u@sy2rcjh(@n=zzxz#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
